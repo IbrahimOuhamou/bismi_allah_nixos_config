d5ab2f6d49fa3b47174e8ae3607e323f20e162fc
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-04-10 09:11:15 +0000

    alhamdo li Allah added mysqlclient to python packages

--- dev_envs/python3.nix
+++ dev_envs/python3.nix
@@ -6,7 +6,7 @@ in pkgs.mkShell {
     (pkgs.python3.withPackages (python-pkgs: [
       python-pkgs.tkinter
       python-pkgs.django
-      python-pkgs.pillow
+      python-pkgs.mysqlclient
     ]))
   ];
 }
