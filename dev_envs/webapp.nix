2bf40b0e457044ef2369ecf02ca5b5f93ee6600f
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-04-26 09:48:09 +0100

    alhamdo li Allah

--- dev_envs/webapp.nix
+++ dev_envs/webapp.nix
@@ -1,9 +1,11 @@
 #in the name of Allah
-{ pkgs ? import <nixpkgs> {} }:
-{
-  services.mysql =
-  {
-    enable = true;
-    package = pkgs.mariadb;
-  };
+with import <nixpkgs> {};
+stdenv.mkDerivation {
+  name = "react-bootstrap-shell";
+  buildInputs = with pkgs; [
+    nodePackages.create-react-app
+    create-react-app
+    nodejs
+    yarn
+  ];
 }
