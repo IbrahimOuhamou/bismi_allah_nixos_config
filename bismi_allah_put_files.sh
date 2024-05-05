bfb3dd81f4354ce5475e53be84466797db22dd72
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-05-05 07:29:18 +0100

    alhamdo li Allah

--- bismi_allah_put_files.sh
+++ bismi_allah_put_files.sh
@@ -11,22 +11,23 @@ echo ""
 
 echo "will copy the dev_envs by the will of Allah"
 mkdir ~/helpers
-cp -r ./dev_envs ~/helpers
+cp -r ./dev_envs/* ~/helpers/
 
 echo "will copy sway's config by the will of Allah"
 mkdir ~/.config/sway
-cp -r ./sway ~/.config/sway/
+cp -r ./sway/* ~/.config/sway/
 
 echo "will copy foot's config by the will of Allah"
 mkdir ~/.config/foot
-cp -r ./foot ~/.config/foot/
+cp -r ./foot/* ~/.config/foot/
 
 echo "will copy waybar's config by the will of Allah"
 mkdir ~/.config/waybar
-cp -r ./waybar ~/.config/waybar/
+cp -r ./waybar/* ~/.config/waybar/
 
 echo "will copy wofi's config by the will of Allah"
-cp -r ./wofi ~/.config/wofi/
+mkdir ~/.config/wofi
+cp -r ./wofi/* ~/.config/wofi/
 
 
 
