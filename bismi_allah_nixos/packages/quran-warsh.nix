6e90a4741bf7435e940b3f35afc1601c50e10de2
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-07-17 20:17:59 +0100

    alhamdo li Allah

---
+++ bismi_allah_nixos/packages/quran-warsh.nix
@@ -0,0 +1,32 @@
+# بسم الله الرحمن الرحيم
+# la ilaha illa Allah Mohammed Rassoul Allah
+{ lib
+, stdenv
+, fetchgit
+, zig
+, csfml
+}:
+
+stdenv.mkDerivation {
+  pname = "quran-warsh";
+  version = "alpha-2";
+
+  src = fetchgit {
+    url = "https://github.com/muslimDevCommunity/quran-warsh.git";
+    rev = "f7a60ad6e339196b9a9ac9803f16bd685f0c9c9f";
+    hash = "sha256-QXqcFZVMIRPAISA3ezEsleK8oTmhVgb6TWGW6C6nPBc=";
+  };
+
+  nativeBuildInputs = [
+    csfml
+    zig.hook
+  ];
+
+  meta = with lib; {
+    description = "warsh tajweed quran for desktop";
+    homepage = "https://github.com/muslimDevCommunity/quran-warsh";
+    # license = licenses.mit;
+    mainProgram = "quran-warsh";
+    inherit (zig.meta) platforms;
+  };
+}
