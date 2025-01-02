ba9fffa79645844712e97743bfec9fb4a7f8cc95
IbrahimOuhamou <ouhamouy10@gmail.com>
2025-01-02 08:01:53 +0100

    alhamdo li Allah

--- dev_envs/postgres.nix
+++ dev_envs/postgres.nix
@@ -10,6 +10,9 @@ mkShell {
     initdb -D /tmp/postresql/bismi_allah_db
     pg_ctl -D /tmp/postresql/bismi_allah_db -l /tmp/postresql/logfile start
 
+    psql -U ibrahimo -d postgres -c "CREATE ROLE postgres WITH SUPERUSER LOGIN PASSWORD 'bismi_allah';"
+    psql -U postgres -d postgres -c "ALTER USER postgres WITH PASSWORD 'bismi_allah';"
+
     trap "pg_ctl -D /tmp/postresql/bismi_allah_db stop" EXIT
   '';
 }
