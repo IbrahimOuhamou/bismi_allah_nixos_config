8b0ba4eb6b9ab62fd26ea148c256afbaf0f226b6
IbrahimOuhamou <ouhamouy10@gmail.com>
2025-02-21 09:15:42 +0100

    alhamdo li Allah

--- dev_envs/webapp.nix
+++ dev_envs/webapp.nix
@@ -4,10 +4,7 @@ with import <nixpkgs> {};
 stdenv.mkDerivation {
   name = "react-bootstrap-shell";
   buildInputs = with pkgs; [
-    nodePackages.create-react-app
-    create-react-app
     nodejs
-    yarn
   ];
 
   shellHook = ''
